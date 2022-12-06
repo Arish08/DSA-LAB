943e03e7056a9c94f5f808b9a2ec845d723c5d35
Arish <91873754+Arish08@users.noreply.github.com>
2022-12-06 23:57:17 +0500

    Add files via upload

---
+++ untitled9/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+        Sll sll = new Sll();
+        sll.insertfirst(5);
+        sll.insertfirst(6);
+        sll.insertfirst(8);
+        sll.insertfirst(7);
+        sll.insertfirst(2);
+        sll.insertlast(8);
+        sll.deletefirst();
+        sll.deletelast();
+
+sll.displaylist();
+
+
+
+
+
+
+
+
+
+    }
+
+}
